cd599b4ef5b2dfa72c770ae9dea4b87f2463d7f6
Ai Nguyen <ainguyenkaka@gmail.com>
2018-01-13 16:45:00 +0800

    optimize dataset

--- src/main/webapp/app/blocks/handlers/state.handler.js
+++ src/main/webapp/app/blocks/handlers/state.handler.js
@@ -32,7 +32,7 @@
             });
 
             var stateChangeSuccess = $rootScope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-                var titleKey = 'visualApp';
+                var titleKey = 'StatNLP Playground';
 
                 // Set the page title key to the one configured in state or use default one
                 if (toState.data.pageTitle) {
